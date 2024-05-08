e95dcbcb6cc1164a8e13bd6d129ce1df3fcff3be
Ashish Thotwe <ashishthotwe20@gmail.com>
2024-05-08 08:31:40 +0530

    Frontend Part Completed

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,96 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import { counts } from "../utils/data";
+import CountsCard from "../components/cards/CountsCard";
+import WeeklyStatCard from "../components/cards/WeeklyStatCard";
+import CategoryChart from "../components/cards/CategoryChart";
+// import AddWorkout from "../components/AddWorkout";
+import WorkoutCard from "../components/cards/WorkoutCard";
 
-export default function Dashboard() {
+const Container = styled.div`
+  flex: 1;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  padding: 22px 0px;
+  overflow-y: scroll;
+`;
+const Wrapper = styled.div`
+  flex: 1;
+  max-width: 1400px;
+  display: flex;
+  flex-direction: column;
+  gap: 22px;
+  @media (max-width: 600px) {
+    gap: 12px;
+  }
+`;
+const Title = styled.div`
+  padding: 0px 16px;
+  font-size: 22px;
+  color: ${({ theme }) => theme.text_primary};
+  font-weight: 500;
+`;
+const FlexWrap = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  gap: 22px;
+  padding: 0px 16px;
+  @media (max-width: 600px) {
+    gap: 12px;
+  }
+`;
+const Section = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 0px 16px;
+  gap: 22px;
+  padding: 0px 16px;
+  @media (max-width: 600px) {
+    gap: 12px;
+  }
+`;
+const CardWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 20px;
+  margin-bottom: 100px;
+  @media (max-width: 600px) {
+    gap: 12px;
+  }
+`;
+
+const Dashboard = () => {
   return (
-    <div>Dashboard</div>
-  )
-}
+    <Container>
+      <Wrapper>
+        <Title>Dashboard</Title>
+        <FlexWrap>
+          {counts.map((item, index) => (
+            <CountsCard key={index} item={item} />
+          ))}
+        </FlexWrap>
+
+        <FlexWrap>
+          <WeeklyStatCard />
+          <CategoryChart />
+          {/* <AddWorkout /> */}
+        </FlexWrap>
+
+        <Section>
+          <Title>Todays Workouts</Title>
+          <CardWrapper>
+            <WorkoutCard />
+            <WorkoutCard />
+            <WorkoutCard />
+           
+          </CardWrapper>
+        </Section>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Dashboard;
